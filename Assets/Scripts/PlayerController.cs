bb53b03b39914d62b5f48b4f4110b664ec17036f
Grouli123 <S00yer@mail.ru>
2023-03-07 01:22:13 +0300

    Create Project and add Parallax and Player controller scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // [SerializeField] private GameManager gameManager;
+    [SerializeField] private float _gravityScale = 1f;
+    private Rigidbody2D _rb;
+    private bool _isGroundBot;
+
+    private void Start()
+    {
+        _isGroundBot = true;
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {        
+        if(Input.GetMouseButtonDown(0) && _isGroundBot == true)
+        {
+            // _rbv_grabityScale = Vector2.up *_gravityScale;
+            _rb.gravityScale = -_gravityScale;
+            _isGroundBot = false;
+        }
+        else if(Input.GetMouseButtonDown(0) && _isGroundBot == false)
+        {
+            _rb.gravityScale =_gravityScale;
+            _isGroundBot = true;
+        }
+        
+    }
+
+    // private void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     gameManager.GameOver();
+    // }
+}
