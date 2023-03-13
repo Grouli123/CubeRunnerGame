6cf7a95a66b0654a4e11d589be7484c762509e81
Grouli123 <S00yer@mail.ru>
2023-03-13 21:37:02 +0300

    Add asset for stats board

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,19 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+   [HideInInspector] public bool isGameOver;
    [SerializeField] private GameObject _boardScore;
-
    [SerializeField] private GameObject _startButton;
-
    [SerializeField] private GameObject _spawner;
-
    [SerializeField] private SpikeSpawner _spikeSpawner;
 
-   [HideInInspector] public bool isGameOver;
+   [SerializeField] private GameObject _openBoardStats;
+   [SerializeField] private GameObject _statsButton;
+
    public Animator _animatorHit;
 
     private void Start()
     {        
+        _statsButton.SetActive(true);
+        _openBoardStats.SetActive(false);
         _animatorHit.GetComponent<Animator>();
         _spawner.SetActive(false);
         Time.timeScale = 0;
@@ -37,11 +39,24 @@ public class GameManager : MonoBehaviour
 
     public void StartGame()
     {  
+        _statsButton.SetActive(false);
         _animatorHit.SetBool("IsDinoRun", true);
         _spikeSpawner.isTest = true; 
         _spawner.SetActive(true);     
         isGameOver = false;        
         Time.timeScale = 1;
         _startButton.SetActive(false);  
+    }
+
+    public void OpenStatsBoard()
+    {
+        _openBoardStats.SetActive(true);
+        _statsButton.SetActive(false);
+    }
+
+    public void CloseStatsBoard()
+    {
+        _openBoardStats.SetActive(false);
+        _statsButton.SetActive(true);
     }
 }
